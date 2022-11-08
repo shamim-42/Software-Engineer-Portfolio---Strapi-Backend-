27650e74e79de7d4bfb3068a6c1d8e338e0574c3
abusaid7388 <abu.said@tetonelectronics.com>
2022-11-08 16:02:46 +0600

    AWS s3 apply

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,19 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'shamims-world.s3.ap-southeast-1.amazonaws.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'shamims-world.s3.ap-southeast-1.amazonaws.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
